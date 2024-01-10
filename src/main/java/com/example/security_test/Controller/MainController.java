f5661f10ca6f369ddd5d66a8cbdaaf41902f7bb8
pdill <dbtmzp1@naver.com>
2024-01-10 22:49:51 +0900

    spring_security 학습 프로젝트 생성

---
+++ src/main/java/com/example/security_test/Controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.security_test.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    
+    @GetMapping("/")
+    public String MainP(){
+        
+        return "main";
+    }
+    
+}
